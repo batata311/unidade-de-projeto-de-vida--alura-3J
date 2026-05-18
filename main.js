e8377276570afd02f19b12469e4af07eaada80a8
batata311 <manuella.ferst@escola.pr.gov.br>
2026-05-18 10:59:12 +0000

    aula 1, 2 e metade da aula 3

---
+++ main.js
@@ -0,0 +1,25 @@
+const botoes = document.querySelectorAll(".botao");
+const abas = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      abas[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    abas[i].classList.add("ativo");
+  };
+}
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
+}
