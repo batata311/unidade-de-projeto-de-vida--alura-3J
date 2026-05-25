cba0b1ee63bb7fab23f7a49048108f469d3fdeef
batata311 <manuella.ferst@escola.pr.gov.br>
2026-05-25 10:51:52 +0000

    alteração nas cores e datas

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-11-01T00:00");
-const tempoObjetivo2 = new Date("2026-11-15T00:00");
-const tempoObjetivo3 = new Date("2026-12-05T00:00");
-const tempoObjetivo4 = new Date("2026-12-13T00:00");
+const tempoObjetivo2 = new Date("2026-11-08T00:00");
+const tempoObjetivo3 = new Date("2026-11-15T00:00");
+const tempoObjetivo4 = new Date("2027-01-16T00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
